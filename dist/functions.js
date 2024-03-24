31533b1bfbe1852953a027211b966ed5f9522e45
iftekharweb <iftekharweb@gmail.com>
2024-03-24 13:24:28 +0600

    TypeScript Functions

--- dist/functions.js
+++ dist/functions.js
@@ -5,3 +5,14 @@ function bonus(income) {
     }
     return income;
 }
+bonus(20000);
+function CalculatedTax(income, taxYear = 2022) {
+    if (taxYear < 2022) {
+        return income * 1.2;
+    }
+    return income;
+}
+let Tax = CalculatedTax(20100);
+console.log(Tax);
+Tax = CalculatedTax(20100, 1999);
+console.log(Tax);
--- src/functions.ts
+++ src/functions.ts
@@ -5,3 +5,17 @@ function bonus(income: number): number {
     }
     return income;
 }
+
+bonus(20_000);
+
+function CalculatedTax(income:number, taxYear:number = 2022) : number {
+    if(taxYear<2022) {
+        return income*1.2;
+    }
+    return income
+}
+
+let Tax:number = CalculatedTax(20_100);
+console.log(Tax);
+Tax = CalculatedTax(20_100, 1999);
+console.log(Tax);
