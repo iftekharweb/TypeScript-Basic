066c47247e3fdb62832f41aab7765637681ad909
iftekharweb <iftekharweb@gmail.com>
2024-03-24 17:55:09 +0600

    TypeScript Tuples

---
+++ dist/tuples.js
@@ -0,0 +1,5 @@
+"use strict";
+const user = [1, 'Shishir'];
+console.log(user);
+const info = [1, 'shishir', 25, false];
+console.log(info);
---
+++ src/tuples.ts
@@ -0,0 +1,10 @@
+
+// Actually an array of fiexd length
+
+const user:[number, string] = [1, 'Shishir']
+
+console.log(user)
+
+const info:[number, string, number, boolean] = [1, 'shishir', 25, false]
+
+console.log(info)
