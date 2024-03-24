14913947b6df35f3a7633468c96437d5eee5fba5
iftekharweb <iftekharweb@gmail.com>
2024-03-24 14:03:09 +0600

    TypeScript Arrays

---
+++ dist/arrays.js
@@ -0,0 +1,7 @@
+"use strict";
+let arr = [10, 23, 45];
+let anyArr = [100, 'shishir'];
+let strArr = ['shsihir', 'Arshad', 'Ohid', 'Sujat'];
+console.log(arr);
+console.log(anyArr);
+console.log(strArr);
---
+++ src/arrays.ts
@@ -0,0 +1,13 @@
+
+// of type numbers
+let arr: number[] = [10,23, 45];
+
+// of type any
+let anyArr = [100, 'shishir']
+
+// of type string
+let strArr: string[] = ['shsihir', 'Arshad', 'Ohid', 'Sujat']
+
+console.log(arr)
+console.log(anyArr)
+console.log(strArr)
